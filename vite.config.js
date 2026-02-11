8ffc2e6a0755ad205029ae4c10f9043095eb9959
Shadi El Sangedy <shadielsangedy@Shadis-MacBook-Pro.local>
2026-02-11 23:31:02 +0100

    fix vercel react-router conflict for real this time I hope

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
   server: {
-    port: 3000
-  }
-}
-)
-
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+  },
+})
